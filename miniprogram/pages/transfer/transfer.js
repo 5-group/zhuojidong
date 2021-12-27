48cad679fa4ce7763f0b52eeebc440fae6eaf4ac
jdzhuo <92216304+jdzhuo@users.noreply.github.com>
2021-12-27 16:50:35 +0800

    Add files via upload

---
+++ miniprogram/pages/transfer/transfer.js
@@ -0,0 +1,27 @@
+Page({
+  jumpto1(){
+    wx.navigateTo({
+      url: '../management/management',
+    })
+  },
+  jumpto2(){
+    wx.navigateTo({
+      url: '../managementin/managementin',
+    })
+  },
+  jumpto3(){
+    wx.navigateTo({
+      url: '../management/management',
+    })
+  },
+  jumpto4(){
+    wx.navigateTo({
+      url: '../management_pro/management_pro',
+    })
+  },
+  jumpto5(){
+    wx.navigateTo({
+      url: '../management_company/management_company',
+    })
+  }
+})
